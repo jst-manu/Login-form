d683eed09b72a9479e65055fa8e28d12bab74695
MANOJ S <95956060+jst-manu@users.noreply.github.com>
2022-10-17 18:38:59 +0530

    Update register.php

--- register.php
+++ register.php
@@ -20,7 +20,7 @@ if (!empty($username2) || !empty($email) || !empty($mobile) || !empty($password2
 
     if(mysqli_connect_error()){
 
-        die('Connect Error ('. mysqli_connect_error() .')' . mysqli_connect_error());
+        die('Connect Error ('. mysqli_connect_errno() .')' . mysqli_connect_error());
 
     }
     else{
